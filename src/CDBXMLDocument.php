7f30110686bf634837f8304fc3bf2a8802d62afd
Bert Ramakers <bertramakers@me.com>
2016-03-29 17:48:38 +0200

    III-845: Implemented CDBXMLDocument.

--- src/CDBXMLDocument.php
+++ src/CDBXMLDocument.php
@@ -2,21 +2,41 @@
 
 namespace CultuurNet\UDB3\CDBXMLService;
 
-class CDBXMLDocument
+use Broadway\ReadModel\ReadModelInterface;
+
+class CDBXMLDocument implements ReadModelInterface
 {
-    
-    
+    /**
+     * @var string
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $cdbxml;
+
+    /**
+     * @param string $id
+     * @param string $cdbxml
+     */
+    public function __construct($id, $cdbxml)
+    {
+        $this->id = $id;
+        $this->cdbxml = $cdbxml;
+    }
+
     /**
      * @return string
      */
     public function getId() {
-
+        return $this->id;
     }
 
     /**
      * @return string
      */
     public function getCDBXML() {
-
+        return $this->cdbxml;
     }
 }
