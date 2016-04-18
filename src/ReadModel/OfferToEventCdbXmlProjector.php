e8eeac333850d82cff3bf79c160cfc375c515fc2
Nicolas Leroy <nicolas.leroy@gmail.com>
2016-04-18 17:10:08 +0200

    III-878: Add a first apply method to the OfferToEvent Projector

--- src/ReadModel/OfferToEventCdbXmlProjector.php
+++ src/ReadModel/OfferToEventCdbXmlProjector.php
@@ -3,10 +3,37 @@
 namespace CultuurNet\UDB3\CdbXmlService\ReadModel;
 
 use Broadway\Domain\DomainMessage;
+use Broadway\Domain\Metadata;
 use Broadway\EventHandling\EventListenerInterface;
+use CultureFeed_Cdb_Data_Address;
+use CultureFeed_Cdb_Data_Address_PhysicalAddress;
+use CultureFeed_Cdb_Data_Calendar_OpeningTime;
+use CultureFeed_Cdb_Data_Calendar_Period;
+use CultureFeed_Cdb_Data_Calendar_PeriodList;
+use CultureFeed_Cdb_Data_Calendar_Permanent;
+use CultureFeed_Cdb_Data_Calendar_SchemeDay;
+use CultureFeed_Cdb_Data_Calendar_Timestamp;
+use CultureFeed_Cdb_Data_Calendar_TimestampList;
+use CultureFeed_Cdb_Data_Calendar_Weekscheme;
+use CultureFeed_Cdb_Data_Category;
+use CultureFeed_Cdb_Data_CategoryList;
+use CultureFeed_Cdb_Data_ContactInfo;
+use CultureFeed_Cdb_Data_EventDetail;
+use CultureFeed_Cdb_Data_EventDetailList;
+use CultureFeed_Cdb_Data_Location;
+use CultureFeed_Cdb_Item_Event;
+use CultuurNet\UDB3\Calendar;
+use CultuurNet\UDB3\CalendarInterface;
 use CultuurNet\UDB3\CdbXmlService\CdbXmlPublisherInterface;
 use CultuurNet\UDB3\CdbXmlService\NullCdbXmlPublisher;
+use CultuurNet\UDB3\CdbXmlService\ReadModel\Repository\CdbXmlDocumentFactoryInterface;
 use CultuurNet\UDB3\CdbXmlService\ReadModel\Repository\DocumentRepositoryInterface;
+use CultuurNet\UDB3\Event\Events\EventCreated;
+use CultuurNet\UDB3\Event\Events\TitleTranslated as EventTitleTranslated;
+use CultuurNet\UDB3\Location;
+use CultuurNet\UDB3\Offer\Events\AbstractTitleTranslated;
+use CultuurNet\UDB3\Place\Events\TitleTranslated as PlaceTitleTranslated;
+use CultuurNet\UDB3\PlaceService;
 
 /**
  * Class OfferToEventCdbXmlProjector
@@ -21,6 +48,21 @@ class OfferToEventCdbXmlProjector implements EventListenerInterface
     private $documentRepository;
 
     /**
+     * @var CdbXmlDocumentFactoryInterface
+     */
+    private $cdbXmlDocumentFactory;
+
+    /**
+     * @var PlaceService
+     */
+    protected $placeService;
+
+    /**
+     * @var MetadataCdbItemEnricherInterface
+     */
+    private $metadataCdbItemEnricher;
+
+    /**
      * @var CdbXmlPublisherInterface
      */
     private $cdbXmlPublisher;
@@ -29,9 +71,15 @@ class OfferToEventCdbXmlProjector implements EventListenerInterface
      * @param DocumentRepositoryInterface $documentRepository
      */
     public function __construct(
-        DocumentRepositoryInterface $documentRepository
+        DocumentRepositoryInterface $documentRepository,
+        CdbXmlDocumentFactoryInterface $cdbXmlDocumentFactory,
+        PlaceService $placeService,
+        MetadataCdbItemEnricherInterface $metadataCdbItemEnricher
     ) {
         $this->documentRepository = $documentRepository;
+        $this->cdbXmlDocumentFactory = $cdbXmlDocumentFactory;
+        $this->placeService = $placeService;
+        $this->metadataCdbItemEnricher = $metadataCdbItemEnricher;
         $this->cdbXmlPublisher = new NullCdbXmlPublisher();
     }
 
@@ -51,5 +99,257 @@ class OfferToEventCdbXmlProjector implements EventListenerInterface
      */
     public function handle(DomainMessage $domainMessage)
     {
+        $payload = $domainMessage->getPayload();
+        $payloadClassName = get_class($payload);
+
+        $metadata = $domainMessage->getMetadata();
+
+        $handlers = [
+            EventTitleTranslated::class => 'applyTitleTranslated',
+            PlaceTitleTranslated::class => 'applyTitleTranslated',
+            EventCreated::class => 'applyEventCreated',
+        ];
+
+        if (isset($handlers[$payloadClassName])) {
+            $handler = $handlers[$payloadClassName];
+            $cdbXmlDocument = $this->{$handler}($payload, $metadata);
+
+            $this->documentRepository->save($cdbXmlDocument);
+
+            $this->cdbXmlPublisher->publish($cdbXmlDocument, $domainMessage);
+        }
+    }
+
+    public function applyEventCreated(
+        EventCreated $eventCreated,
+        Metadata $metadata
+    ) {
+        $event = new CultureFeed_Cdb_Item_Event();
+        $event->setCdbId($eventCreated->getEventId());
+
+        $nlDetail = new CultureFeed_Cdb_Data_EventDetail();
+        $nlDetail->setLanguage('nl');
+        $nlDetail->setTitle($eventCreated->getTitle());
+
+        $details = new CultureFeed_Cdb_Data_EventDetailList();
+        $details->add($nlDetail);
+        $event->setDetails($details);
+
+        // Set location and calendar info.
+        $this->setLocation($eventCreated->getLocation(), $event);
+        $this->setCalendar($eventCreated->getCalendar(), $event);
+
+        // Set event type and theme.
+        $event->setCategories(new CultureFeed_Cdb_Data_CategoryList());
+        $eventType = new CultureFeed_Cdb_Data_Category(
+            'eventtype',
+            $eventCreated->getEventType()->getId(),
+            $eventCreated->getEventType()->getLabel()
+        );
+        $event->getCategories()->add($eventType);
+
+        if ($eventCreated->getTheme() !== null) {
+            $theme = new CultureFeed_Cdb_Data_Category(
+                'theme',
+                $eventCreated->getTheme()->getId(),
+                $eventCreated->getTheme()->getLabel()
+            );
+            $event->getCategories()->add($theme);
+        }
+
+        // Empty contact info.
+        $contactInfo = new CultureFeed_Cdb_Data_ContactInfo();
+        $event->setContactInfo($contactInfo);
+
+        // Add metadata like createdby, creationdate, etc to the actor.
+        $event = $this->metadataCdbItemEnricher
+            ->enrich($event, $metadata);
+
+        // Return a new CdbXmlDocument.
+        return $this->cdbXmlDocumentFactory
+            ->fromCulturefeedCdbItem($event);
+    }
+
+//    public function applyTitleTranslated(
+//        AbstractTitleTranslated $titleTranslated,
+//        Metadata $metadata
+//    ) {
+//
+//    }
+
+    /**
+     * Set the location on the cdb event based on an eventCreated event.
+     *
+     * @param Location $eventLocation
+     * @param CultureFeed_Cdb_Item_Event $cdbEvent
+     * @throws \CultuurNet\UDB3\EntityNotFoundException
+     */
+    private function setLocation(Location $eventLocation, CultureFeed_Cdb_Item_Event $cdbEvent)
+    {
+        $placeEntity = $this->placeService->getEntity($eventLocation->getCdbid());
+        $place = json_decode($placeEntity);
+
+        $physicalAddress = new CultureFeed_Cdb_Data_Address_PhysicalAddress();
+        $physicalAddress->setCountry($place->address->addressCountry);
+        $physicalAddress->setCity($place->address->addressLocality);
+        $physicalAddress->setZip($place->address->postalCode);
+
+
+        // @todo This is not an exact mapping, because we do not have a separate
+        // house number in JSONLD, this should be fixed somehow. Probably it's
+        // better to use another read model than JSON-LD for this purpose.
+        $streetParts = explode(' ', $place->address->streetAddress);
+
+        if (count($streetParts) > 1) {
+            $number = array_pop($streetParts);
+            $physicalAddress->setStreet(implode(' ', $streetParts));
+            $physicalAddress->setHouseNumber($number);
+        } else {
+            $physicalAddress->setStreet($eventLocation->getStreet());
+        }
+
+        $address = new CultureFeed_Cdb_Data_Address($physicalAddress);
+
+        $location = new CultureFeed_Cdb_Data_Location($address);
+        $location->setLabel($eventLocation->getName());
+        $cdbEvent->setLocation($location);
+    }
+
+    /**
+     * Set the Calendar on the cdb event.
+     *
+     * @param CalendarInterface $eventCalendar
+     * @param CultureFeed_Cdb_Item_Event $cdbEvent
+     */
+    public function setCalendar(CalendarInterface $eventCalendar, CultureFeed_Cdb_Item_Event $cdbEvent)
+    {
+        // Store opening hours.
+        $openingHours = $eventCalendar->getOpeningHours();
+        $weekScheme = null;
+
+        if (!empty($openingHours)) {
+            // CDB2 requires an entry for every day.
+            $requiredDays = array(
+                'monday',
+                'tuesday',
+                'wednesday',
+                'thursday',
+                'friday',
+                'saturday',
+                'sunday',
+            );
+            $weekscheme = new CultureFeed_Cdb_Data_Calendar_Weekscheme();
+
+            // Multiple opening times can happen on same day. Store them in array.
+            $openingTimesPerDay = array(
+                'monday' => array(),
+                'tuesday' => array(),
+                'wednesday' => array(),
+                'thursday' => array(),
+                'friday' => array(),
+                'saturday' => array(),
+                'sunday' => array(),
+            );
+
+            foreach ($openingHours as $openingHour) {
+                // In CDB2 every day needs to be a seperate entry.
+                foreach ($openingHour->dayOfWeek as $day) {
+                    $openingTimesPerDay[$day][] = new CultureFeed_Cdb_Data_Calendar_OpeningTime(
+                        $openingHour->opens . ':00',
+                        $openingHour->closes . ':00'
+                    );
+                }
+
+            }
+
+            // Create the opening times correctly
+            foreach ($openingTimesPerDay as $day => $openingTimes) {
+                // Empty == closed.
+                if (empty($openingTimes)) {
+                    $openingInfo = new CultureFeed_Cdb_Data_Calendar_SchemeDay(
+                        $day,
+                        CultureFeed_Cdb_Data_Calendar_SchemeDay::SCHEMEDAY_OPEN_TYPE_CLOSED
+                    );
+                } else {
+                    // Add all opening times.
+                    $openingInfo = new CultureFeed_Cdb_Data_Calendar_SchemeDay(
+                        $day,
+                        CultureFeed_Cdb_Data_Calendar_SchemeDay::SCHEMEDAY_OPEN_TYPE_OPEN
+                    );
+                    foreach ($openingTimes as $openingTime) {
+                        $openingInfo->addOpeningTime($openingTime);
+                    }
+                }
+
+                $weekscheme->setDay($day, $openingInfo);
+            }
+
+        }
+
+        // Multiple days.
+        if ($eventCalendar->getType() == Calendar::MULTIPLE) {
+            $calendar = new CultureFeed_Cdb_Data_Calendar_TimestampList();
+            foreach ($eventCalendar->getTimestamps() as $timestamp) {
+                $startdate = strtotime($timestamp->getStartDate());
+                $enddate = strtotime($timestamp->getEndDate());
+                $startHour = date('H:i:s', $startdate);
+                if ($startHour == '00:00:00') {
+                    $startHour = null;
+                }
+                $endHour = date('H:i:s', $enddate);
+                if ($endHour == '00:00:00') {
+                    $endHour = null;
+                }
+                $calendar->add(
+                    new CultureFeed_Cdb_Data_Calendar_Timestamp(
+                        date('Y-m-d', $startdate),
+                        $startHour,
+                        $endHour
+                    )
+                );
+            }
+            // Single day.
+        } elseif ($eventCalendar->getType() == Calendar::SINGLE) {
+            $calendar = new CultureFeed_Cdb_Data_Calendar_TimestampList();
+            $startdate = strtotime($eventCalendar->getStartDate());
+            $enddate = strtotime($eventCalendar->getEndDate());
+            $startHour = date('H:i:s', $startdate);
+            if ($startHour == '00:00:00') {
+                $startHour = null;
+            }
+            $endHour = date('H:i:s', $enddate);
+            if ($endHour == '00:00:00') {
+                $endHour = null;
+            }
+            $calendar->add(
+                new CultureFeed_Cdb_Data_Calendar_Timestamp(
+                    date('Y-m-d', $startdate),
+                    $startHour,
+                    $endHour
+                )
+            );
+            // Period.
+        } elseif ($eventCalendar->getType() == Calendar::PERIODIC) {
+            $calendar = new CultureFeed_Cdb_Data_Calendar_PeriodList();
+            $startdate = date('Y-m-d', strtotime($eventCalendar->getStartDate()));
+            $enddate = date('Y-m-d', strtotime($eventCalendar->getEndDate()));
+
+            $period = new CultureFeed_Cdb_Data_Calendar_Period($startdate, $enddate);
+            if (!empty($weekScheme)) {
+                $calendar->setWeekScheme($weekscheme);
+            }
+            $calendar->add($period);
+
+            // Permanent
+        } elseif ($eventCalendar->getType() == Calendar::PERMANENT) {
+            $calendar = new CultureFeed_Cdb_Data_Calendar_Permanent();
+            if (!empty($weekScheme)) {
+                $calendar->setWeekScheme($weekscheme);
+            }
+
+        }
+
+        $cdbEvent->setCalendar($calendar);
+
     }
 }
