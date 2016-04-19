43f32ecb8efb59b75ad3559807f70af248ef050d
Bert Ramakers <bertramakers@me.com>
2016-04-19 17:33:27 +0200

    III-836: Reverted incorrect merge of OfferToEventCdbXmlProjector.

--- src/ReadModel/OfferToEventCdbXmlProjector.php
+++ src/ReadModel/OfferToEventCdbXmlProjector.php
@@ -5,28 +5,51 @@ namespace CultuurNet\UDB3\CdbXmlService\ReadModel;
 use Broadway\Domain\DomainMessage;
 use Broadway\EventHandling\EventListenerInterface;
 use CultuurNet\UDB3\CdbXmlService\CdbXmlPublisherInterface;
+use CultuurNet\UDB3\CdbXmlService\NullCdbXmlPublisher;
 use CultuurNet\UDB3\CdbXmlService\ReadModel\Repository\DocumentRepositoryInterface;
 
 /**
  * Class OfferToEventCdbXmlProjector
- *  This projector takes UDB3 domain messages and projects them to CDBXML and
- *  then publishes the changes to a public URL.
- *
- * @package CultuurNet\UDB3\CdbXmlService
+ * This projector takes UDB3 domain messages, projects them to CdbXml and then
+ * publishes the changes to a public URL.
  */
 class OfferToEventCdbXmlProjector implements EventListenerInterface
 {
     /**
-     * OfferToEventCdbXmlProjector constructor.
+     * @var DocumentRepositoryInterface
+     */
+    private $documentRepository;
+
+    /**
+     * @var CdbXmlPublisherInterface
+     */
+    private $cdbXmlPublisher;
+
+    /**
+     * @param DocumentRepositoryInterface $documentRepository
      */
     public function __construct(
-        DocumentRepositoryInterface $documentRepositoryInterface,
-        CdbXmlPublisherInterface $CDBXMLPublisher
+        DocumentRepositoryInterface $documentRepository
     ) {
+        $this->documentRepository = $documentRepository;
+        $this->cdbXmlPublisher = new NullCdbXmlPublisher();
     }
 
+    /**
+     * @param CdbXmlPublisherInterface $cdbXmlPublisher
+     * @return OfferToEventCdbXmlProjector
+     */
+    public function withCdbXmlPublisher(CdbXmlPublisherInterface $cdbXmlPublisher)
+    {
+        $c = clone $this;
+        $c->cdbXmlPublisher = $cdbXmlPublisher;
+        return $c;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
     public function handle(DomainMessage $domainMessage)
     {
-        // TODO: Implement handle() method.
     }
 }
