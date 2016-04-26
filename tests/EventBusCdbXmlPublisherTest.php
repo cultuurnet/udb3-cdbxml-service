58f469b375a47ea9000e3d886a7523a52f15ae23
Bram Cordie <bramcordie@gmail.com>
2016-04-26 22:13:55 +0200

    III-910: Get publication location from metadata instead of generating

--- tests/EventBusCdbXmlPublisherTest.php
+++ tests/EventBusCdbXmlPublisherTest.php
@@ -13,7 +13,6 @@ use CultuurNet\UDB3\CdbXmlService\ReadModel\Repository\CdbXmlDocument;
 use CultuurNet\UDB3\Event\Events\EventCreated;
 use CultuurNet\UDB3\Event\Events\TitleTranslated;
 use CultuurNet\UDB3\Event\EventType;
-use CultuurNet\UDB3\Iri\IriGeneratorInterface;
 use CultuurNet\UDB3\Language;
 use CultuurNet\UDB3\Location;
 use CultuurNet\UDB3\Organizer\Events\OrganizerCreated;
@@ -25,10 +24,6 @@ use ValueObjects\String\String as StringLiteral;
 
 class EventBusCdbXmlPublisherTest extends \PHPUnit_Framework_TestCase
 {
-    /**
-     * @var IriGeneratorInterface|\PHPUnit_Framework_MockObject_MockObject
-     */
-    protected $iriGenerator;
 
     /**
      * @var EventBusInterface|\PHPUnit_Framework_MockObject_MockObject
@@ -43,10 +38,8 @@ class EventBusCdbXmlPublisherTest extends \PHPUnit_Framework_TestCase
     public function setUp()
     {
         $this->eventBus = $this->getMock(EventBusInterface::class);
-        $this->iriGenerator = $this->getMock(IriGeneratorInterface::class);
 
         $this->publisher = new EventBusCdbXmlPublisher(
-            $this->iriGenerator,
             $this->eventBus
         );
     }
@@ -74,16 +67,11 @@ class EventBusCdbXmlPublisherTest extends \PHPUnit_Framework_TestCase
         $originalDomainMessage = new DomainMessage(
             UUID::generateAsString(),
             0,
-            new Metadata(['user_id' => $authorId]),
+            new Metadata(['user_id' => $authorId, 'id' => $publicationUrl]),
             $originalDomainEvent,
             DateTime::fromString($publicationDate)
         );
         $document = $this->getEmptyDocument($documentId);
-
-        $this->iriGenerator
-            ->expects($this->once())
-            ->method('iri')
-            ->willReturn($publicationUrl);
 
         $this->eventBus
             ->expects($this->once())
@@ -126,16 +114,11 @@ class EventBusCdbXmlPublisherTest extends \PHPUnit_Framework_TestCase
         $originalDomainMessage = new DomainMessage(
             UUID::generateAsString(),
             0,
-            new Metadata(),
+            new Metadata(['id' => $publicationUrl]),
             $originalDomainEvent,
             DateTime::fromString($publicationDate)
         );
         $document = $this->getEmptyDocument($documentId);
-
-        $this->iriGenerator
-            ->expects($this->once())
-            ->method('iri')
-            ->willReturn($publicationUrl);
 
         $this->eventBus
             ->expects($this->once())
@@ -162,16 +145,11 @@ class EventBusCdbXmlPublisherTest extends \PHPUnit_Framework_TestCase
     {
         $publicationUrl = 'http://foo.be/item/A59682E1-6745-4AF3-8B7F-FB8A8FE895D5';
 
-        $this->iriGenerator
-            ->expects($this->once())
-            ->method('iri')
-            ->willReturn($publicationUrl);
-
         $originalDomainEvent = new TitleTranslated($publicationUrl, new Language('nl'), new StringLiteral('c'));
         $originalDomainMessage = new DomainMessage(
             UUID::generateAsString(),
             0,
-            new Metadata(['user_id' => 'me-me']),
+            new Metadata(['user_id' => 'me-me', 'id' => $publicationUrl]),
             $originalDomainEvent,
             DateTime::now()
         );
@@ -197,85 +175,37 @@ class EventBusCdbXmlPublisherTest extends \PHPUnit_Framework_TestCase
 
     /**
      * @test
-     *
-     * @dataProvider eventAndUrlProvider
-     *
-     * @param string $itemId
-     * @param DomainMessage $domainMessage
-     * @param $urlSuffix
      */
-    public function it_should_specify_the_type_of_the_item_when_generating_the_public_url(
-        $itemId,
-        DomainMessage $domainMessage,
-        $urlSuffix
-    ) {
-        $this->iriGenerator
-            ->expects($this->once())
-            ->method('iri')
-            ->with($urlSuffix)
-            ->willReturn('http://dirk.de/' . $urlSuffix);
-
-        $this->publisher->publish(
-            $this->getEmptyDocument($itemId),
-            $domainMessage
-        );
-    }
-
-    public function eventAndUrlProvider()
+    public function it_should_publish_the_item_at_the_url_provided_by_the_metadata_id_property()
     {
-        return [
-            [
-                'F5FBFB15-7E38-44A2-96AB-7C1F1364F5D3',
-                new DomainMessage(
-                    UUID::generateAsString(),
-                    0,
-                    new Metadata(['user_id' => 'me-me']),
-                    new TitleTranslated(
-                        'F5FBFB15-7E38-44A2-96AB-7C1F1364F5D3',
-                        new Language('nl'),
-                        new StringLiteral('c')
-                    ),
-                    DateTime::now()
-                ),
-                'event/F5FBFB15-7E38-44A2-96AB-7C1F1364F5D3',
-            ],
-            [
-                'DA899093-F7B8-47B9-AC7D-95A52A793F0E',
-                new DomainMessage(
-                    UUID::generateAsString(),
-                    0,
-                    new Metadata(['user_id' => 'me-me']),
-                    new DescriptionUpdated(
-                        'DA899093-F7B8-47B9-AC7D-95A52A793F0E',
-                        new Language('nl')
-                    ),
-                    DateTime::now()
-                ),
-                'place/DA899093-F7B8-47B9-AC7D-95A52A793F0E',
-            ],
-        ];
-    }
+        $publicationUrl = 'http://foo.be/item/A59682E1-6745-4AF3-8B7F-FB8A8FE895D5';
 
-    /**
-     * @test
-     */
-    public function it_should_not_publish_an_item_when_unable_to_determine_its_offer_type()
-    {
-        $document = $this->getEmptyDocument('2A6959C5-08B9-4FDE-88AD-54C17DA24FCA');
-        $domainMessage = new DomainMessage(
+        $originalDomainEvent = new TitleTranslated($publicationUrl, new Language('nl'), new StringLiteral('c'));
+        $originalDomainMessage = new DomainMessage(
             UUID::generateAsString(),
             0,
-            new Metadata(['user_id' => 'me-me']),
-            new OrganizerCreated('2A6959C5-08B9-4FDE-88AD-54C17DA24FCA', new Title('henk'), [], [], [], [], []),
+            new Metadata(['user_id' => 'me-me', 'id' => $publicationUrl]),
+            $originalDomainEvent,
             DateTime::now()
         );
+        $document = $this->getEmptyDocument('A59682E1-6745-4AF3-8B7F-FB8A8FE895D5');
 
-        $this->setExpectedException(
-            InvalidArgumentException::class,
-            'An offer type could not be determined for the domain-event with class: ' . OrganizerCreated::class
-        );
+        $this->eventBus
+            ->expects($this->once())
+            ->method('publish')
+            ->with(
+                $this->callback(
+                    function ($eventStream) use ($publicationUrl) {
+                        /* @var DomainEventStream $eventStream */
+                        /* @var DomainMessage $domainMessage */
+                        $domainMessage = $eventStream->getIterator()->current();
 
-        $this->publisher->publish($document, $domainMessage);
+                        return $publicationUrl === (string) $domainMessage->getPayload()->getUrl();
+                    }
+                )
+            );
+
+        $this->publisher->publish($document, $originalDomainMessage);
     }
 
     /**
