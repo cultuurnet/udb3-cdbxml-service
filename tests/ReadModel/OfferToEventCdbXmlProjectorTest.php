6c7c7bf81d1b9ec351de95937aba6add3a7801fd
Nick <nick@2dotstwice.be>
2016-04-28 14:58:04 +0200

    III-878: remove obsolete place creation

--- tests/ReadModel/OfferToEventCdbXmlProjectorTest.php
+++ tests/ReadModel/OfferToEventCdbXmlProjectorTest.php
@@ -698,8 +698,6 @@ class OfferToEventCdbXmlProjectorTest extends CdbXmlProjectorTestBase
     {
         $this->createEvent(false);
         $id = '404EE8DE-E828-9C07-FE7D12DC4EB24480';
-
-        $this->createPlace();
         $placeId = 'LOCATION-ABC-123';
 
         // add the major info to the event.
