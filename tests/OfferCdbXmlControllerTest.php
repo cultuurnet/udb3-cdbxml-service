ab5a0bf7621b072a16b2067fa4b2e22fa22f1233
Nick <nick@2dotstwice.be>
2016-05-02 15:31:37 +0200

    fix: check for right contenttype string

--- tests/OfferCdbXmlControllerTest.php
+++ tests/OfferCdbXmlControllerTest.php
@@ -39,7 +39,7 @@ class OfferCdbXmlControllerTest extends \PHPUnit_Framework_TestCase
             ->willReturn(new CdbXmlDocument($cdbid, $offerXml));
 
         $actualResponse = $this->controller->get($cdbid);
-        $expectedResponse = new Response($offerXml, 200, ['Content-Type' => 'xml']);
+        $expectedResponse = new Response($offerXml, 200, ['Content-Type' => 'application/xml']);
 
         $this->assertEquals($expectedResponse, $actualResponse);
     }
@@ -58,7 +58,7 @@ class OfferCdbXmlControllerTest extends \PHPUnit_Framework_TestCase
             ->willReturn(null);
 
         $actualResponse = $this->controller->get($cdbid);
-        $expectedResponse = new Response($problemXml, 404, ['Content-Type' => 'xml']);
+        $expectedResponse = new Response($problemXml, 404, ['Content-Type' => 'application/xml']);
 
         $this->assertEquals($expectedResponse, $actualResponse);
     }
@@ -77,7 +77,7 @@ class OfferCdbXmlControllerTest extends \PHPUnit_Framework_TestCase
             ->willThrowException(new DocumentGoneException());
 
         $actualResponse = $this->controller->get($cdbid);
-        $expectedResponse = new Response($problemXml, 410, ['Content-Type' => 'xml']);
+        $expectedResponse = new Response($problemXml, 410, ['Content-Type' => 'application/xml']);
 
         $this->assertEquals($expectedResponse, $actualResponse);
     }
