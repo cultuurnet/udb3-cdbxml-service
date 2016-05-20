ddaf73537e5fe95e409f85f0c8cfaebbb50eb980
Luc Wollants <luc.wollants@apptite.be>
2016-05-20 12:07:27 +0200

    III-909 MetadataCdbItemEnricherInterface has 2 methods: enrich and enrichTime.

--- src/ReadModel/MetadataCdbItemEnricherInterface.php
+++ src/ReadModel/MetadataCdbItemEnricherInterface.php
@@ -3,6 +3,7 @@
 namespace CultuurNet\UDB3\CdbXmlService\ReadModel;
 
 use Broadway\Domain\Metadata;
+use ValueObjects\Number\Natural;
 
 interface MetadataCdbItemEnricherInterface
 {
@@ -12,4 +13,14 @@ interface MetadataCdbItemEnricherInterface
      * @return \CultureFeed_Cdb_Item_Base
      */
     public function enrich(\CultureFeed_Cdb_Item_Base $cdbItem, Metadata $metadata);
+
+    /**
+     * @param \CultureFeed_Cdb_Item_Base $cdbItem
+     * @param Natural $requestTime
+     * @return mixed
+     */
+    public function enrichTime(
+        \CultureFeed_Cdb_Item_Base $cdbItem,
+        Natural $requestTime
+    );
 }
