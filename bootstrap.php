25a30275688413f1fdd7f3c10fe6c88bb88cec2b
Bert Ramakers <bertramakers@me.com>
2016-07-05 12:52:08 +0200

    III-1107: Fix label content types.

--- bootstrap.php
+++ bootstrap.php
@@ -407,7 +407,7 @@ $app['deserializer_locator'] = $app->share(
         $maps =
             \CultuurNet\UDB3\Event\Events\ContentTypes::map() +
             \CultuurNet\UDB3\Place\Events\ContentTypes::map() +
-            \CultuurNet\UDB3\Label\Events\CONTENTTYPES +
+            \CultuurNet\UDB3\Label\Events\ContentTypes::map() +
             \CultuurNet\UDB3\Organizer\Events\ContentTypes::map();
 
         foreach ($maps as $payloadClass => $contentType) {
