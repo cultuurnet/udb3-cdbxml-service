c7b1fc52dbb7c70ec811646534edd5e42f4afdd4
Jonas Drieghe <jonasd@madewithlove.be>
2020-06-02 19:49:16 +0200

    Remove unused methods

--- src/ValueObjects/LabelCollection.php
+++ src/ValueObjects/LabelCollection.php
@@ -79,56 +79,6 @@ class LabelCollection implements \Countable
     }
 
     /**
-     * @param LabelCollection $labelCollectionToMerge
-     * @return LabelCollection
-     */
-    public function merge(LabelCollection $labelCollectionToMerge)
-    {
-        $mergedLabels = [];
-
-        // Keep labels from the original collection that are not in the
-        // collection to merge.
-        foreach ($this->labels as $label) {
-            if (!$labelCollectionToMerge->contains($label)) {
-                $mergedLabels[] = $label;
-            }
-        }
-
-        // Add all labels from the collection to merge.
-        $add = $labelCollectionToMerge->asArray();
-        $mergedLabels = array_merge($mergedLabels, $add);
-
-        return new LabelCollection($mergedLabels);
-    }
-
-    /**
-     * @param LabelCollection $labelCollection
-     * @return LabelCollection
-     */
-    public function intersect(LabelCollection $labelCollection)
-    {
-        $intersectLabels = array_filter(
-            $this->labels,
-            function ($label) use ($labelCollection) {
-                return $labelCollection->contains($label);
-            }
-        );
-
-        return new LabelCollection($intersectLabels);
-    }
-
-    /**
-     * @param callable $filterFunction
-     * @return LabelCollection
-     */
-    public function filter(callable $filterFunction)
-    {
-        return new LabelCollection(
-            array_filter($this->labels, $filterFunction)
-        );
-    }
-
-    /**
      * @param string[] $strings
      * @return LabelCollection
      */
@@ -145,39 +95,5 @@ class LabelCollection implements \Countable
         }
 
         return $labelCollection;
-    }
-
-    /**
-     * @param CultureFeed_Cdb_Data_Keyword[] $keywords
-     * @return LabelCollection
-     */
-    public static function fromKeywords($keywords)
-    {
-        $labelCollection = new LabelCollection();
-
-        foreach ($keywords as $keyword) {
-            try {
-                $label = new Label($keyword->getValue(), $keyword->isVisible());
-                $labelCollection = $labelCollection->with($label);
-            } catch (\InvalidArgumentException $exception) {
-            }
-        }
-
-        return $labelCollection;
-    }
-
-    /**
-     * @return string[]
-     */
-    public function toStrings()
-    {
-        $labels = array_map(
-            function (Label $label) {
-                return (string) $label;
-            },
-            $this->labels
-        );
-
-        return $labels;
     }
 }
