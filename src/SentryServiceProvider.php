d16481ab1f3e02f0add10f3abc2c809080508389
Luc Wollants <lucwollants@gmail.com>
2020-09-24 12:18:49 +0200

    III-3386 Add the Sentry provider

---
+++ src/SentryServiceProvider.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\CdbXmlService;
+
+use Sentry\ClientBuilder;
+use Sentry\State\Hub;
+use Sentry\State\HubInterface;
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+use Throwable;
+
+class SentryServiceProvider implements ServiceProviderInterface
+{
+    public function register(Application $app): void
+    {
+        $app[HubInterface::class] = $app->share(
+            function ($app) {
+                return new Hub(
+                    ClientBuilder::create([
+                        'dsn' => $app['config']['sentry']['dsn'],
+                        'environment' => $app['config']['sentry']['environment'],
+                    ])->getClient()
+                );
+            }
+        );
+
+        $app['uncaught_error_handler'] = $app->share(
+            function ($app) {
+                /** @var HubInterface $sentryHub */
+                $sentryHub = $app[HubInterface::class];
+
+                return static function (Throwable $throwable) use ($sentryHub) {
+                    $sentryHub->captureException($throwable);
+                    throw $throwable;
+                };
+            }
+        );    }
+
+    public function boot(Application $app): void
+    {
+    }
+}
