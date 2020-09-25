5d5ed9536ed405cd324182b164ed387227eca0d5
Luc Wollants <lucwollants@gmail.com>
2020-09-25 09:19:47 +0200

    III-3386 Create concrete error handler for uncaught errors

---
+++ src/Error/UncaughtErrorHandler.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\CdbXmlService\Error;
+
+use Sentry\State\HubInterface;
+
+class UncaughtErrorHandler implements ErrorHandler
+{
+    /** @var HubInterface */
+    private $sentryHub;
+
+    public function __construct(HubInterface $sentryHub)
+    {
+        $this->sentryHub = $sentryHub;
+    }
+
+    public function handle(\Throwable $throwable): void
+    {
+        $this->sentryHub->captureException($throwable);
+        throw $throwable;
+    }
+}
