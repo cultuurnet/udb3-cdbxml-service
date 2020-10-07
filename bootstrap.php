17f35391b9f2a9ff8ebc882fba108a09c2162279
Luc Wollants <lucwollants@gmail.com>
2020-10-07 13:55:13 +0200

    III-3528 Remove obsolete ErrorHandlerProvider

--- bootstrap.php
+++ bootstrap.php
@@ -22,7 +22,6 @@ use CultuurNet\UDB3\CdbXmlService\CdbXmlDocumentController;
 use CultuurNet\UDB3\CdbXmlService\CultureFeed\AddressFactory;
 use CultuurNet\UDB3\CdbXmlService\CultureFeed\FlandersRegionCategoryService;
 use CultuurNet\UDB3\CdbXmlService\DatabaseSchemaInstaller;
-use CultuurNet\UDB3\CdbXmlService\Error\ErrorHandlerProvider;
 use CultuurNet\UDB3\CdbXmlService\EventBusCdbXmlPublisher;
 use CultuurNet\UDB3\CdbXmlService\EventBusRelay;
 use CultuurNet\UDB3\CdbXmlService\Iri\CallableIriGenerator;
@@ -63,7 +62,6 @@ if (!isset($appConfigLocation)) {
 $app->register(new YamlConfigServiceProvider($appConfigLocation . '/config.yml'));
 
 $app->register(new SentryServiceProvider());
-$app->register(new ErrorHandlerProvider());
 
 // Incoming event-stream from UDB3.
 $app['event_bus.udb3-core'] = $app->share(
